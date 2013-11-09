df3aa4c34a3a20e37b3eda5a805237118a7d34de
twardakm <twardakm@gmail.com>
2013-11-09 17:05:25 +0100

    Działający gnuplot

--- main.c
+++ main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define ILOSC_DANYCH 5
+#define ILOSC_KOMEND 2
 
 int main(void)
 {
-    printf("Hello World!\n");
+    char * komendy[] = {"set title \"Generowanie sygnału\"", "plot 'data.temp'"};
+    double x[ILOSC_DANYCH] = {1.0, 2.0, 3.0, 4.0, 5.0};
+    double y[ILOSC_DANYCH] = {5.0, 3.0, 1.0, 3.0, 5.0};
+
+    FILE * temp = fopen("data.temp", "w");
+
+    FILE * gnuplot = popen ("gnuplot -persistent", "w");
+
+    int i;
+
+    for (i = 0; i < ILOSC_DANYCH; i++)
+    {
+        fprintf(temp, "%lf %lf \n", x[i], y[i]);
+    }
+
+    for (i = 0; i < ILOSC_KOMEND; i++)
+    {
+        fprintf(gnuplot, "%s \n", komendy[i]);
+    }
+
+    fflush(gnuplot);
+
     return 0;
 }
 
